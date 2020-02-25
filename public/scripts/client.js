130db87c1974512c2c605e88c0d4f9d72aef9928
Steven Choi <stevencschoi87@gmail.com>
2020-02-25 23:26:18 +0000

    removed debugging code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -52,38 +52,10 @@ const renderTweets = tweets => {
   for (const tweet of tweets) {
     // calls createTweetElement for each tweet
     const tweetEl = createTweetElement(tweet);
-    console.log(tweetEl);
-    console.log(Date.now());
     $(".tweets-container").append(tweetEl);
   }
   // takes return value and appends it to the tweets container
 };
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
 $(function() {
   renderTweets(data);
