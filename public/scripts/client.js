f6519f090e567071cbc8466703cdc29e1b59bd4f
Steven Choi <stevencschoi87@gmail.com>
2020-02-26 19:03:27 +0000

    added loadTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -53,31 +53,6 @@ const renderTweets = tweets => {
   $(".tweets-container").append(markupArray);
 };
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 $(() => {
   $('form').on('submit', event => {
     event.preventDefault();
@@ -85,7 +60,7 @@ $(() => {
     $.ajax({
       url: '/tweets',
       type: 'POST',
-      data: $(this).serialize()
+      dataType: $(this).serialize()
     }).then(tweet => {
       console.log('Success!');
     })
@@ -98,9 +73,9 @@ $(() => {
     $.ajax({
       url: '/tweets',
       type: 'GET',
-      data: 'JSON'
-    })
+      dataType: 'JSON'
+    }).then(result => {
+      renderTweets(result);
+    });
   };
-
-  renderTweets(data);
 });
