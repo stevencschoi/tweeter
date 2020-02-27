ace7c2aa06000244cb6c2a5feb6a3c7c0ecbea8c
Steven Choi <stevencschoi87@gmail.com>
2020-02-27 05:13:03 +0000

    hide appropriate button based on scroll

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,31 +1,30 @@
-$(function() {
+$(() => {
   // stretch toggle button to display new tweet form and scroll up
-  const headerButton = document.getElementsByClassName('.compose');
-  const bottomButton = document.getElementsByClassName('.toggle');
-
   $('.toggle').on('click', () => {
     $('.new-tweet-form').slideDown('slow', resetElements);
     scrollToTop();
-    // $('.toggle').addClass('invisible');
+    $('.toggle').addClass('invisible');
   });
 
-  window.onscroll = function() { showWhichButton() };
+  // listening for scroll on window
+  window.onscroll = function() { showWhichButton();
+  };
   
-  // When the user scrolls down 20px from the top of the document, show the button
+  // if the user scrolls down 50px from the top of the document, hide compose button and show toggle button
   const showWhichButton = () => {
     // accounting for differences in browser compatibility
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      headerButton.addClass('invisible');
-      bottomButton.removeClass('invisible');
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      $('.compose').addClass('invisible');
+      $('.toggle').removeClass('invisible');
     } else {
-      headerButton.removeClass('invisible');
-      bottomButton.addClass('invisible');
+      $('.compose').removeClass('invisible');
+      $('.toggle').addClass('invisible');
     }
-  }
+  };
 
    // When the user clicks on the button, scroll to the top of the document
   const scrollToTop = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-  }
+  };
 });
