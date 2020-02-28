c8e85737bf620ddba1e3ea4fe880152b8d568142
Steven Choi <stevencschoi87@gmail.com>
2020-02-28 19:26:57 +0000

    updated bottom button scroll to more widely supported method

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,32 +1,17 @@
 $(() => {
-  // stretch toggle button to display new tweet form and scroll up
-  $('.toggle').on('click', () => {
-    $('.new-tweet-form').slideDown('slow');
-    scrollToTop();
-    $('.tweet-area').focus();
-    $('.toggle').addClass('invisible');
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+      $('.toggle').fadeIn();
+      $('.compose').fadeOut();
+    } else {
+      $('.toggle').fadeOut();
+      $('.compose').fadeIn();
+    }
   });
 
-  // listening for scroll on window (older function notation as es6 notation does not run this)
-  window.onscroll = function() {
-    showWhichButton();
-  };
-  
-  // if the user scrolls down 50px from the top of the document, hide compose button and show toggle button
-  const showWhichButton = () => {
-    // accounting for differences in browser compatibility
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      $('.compose').addClass('invisible');
-      $('.toggle').removeClass('invisible');
-    } else {
-      $('.compose').removeClass('invisible');
-      $('.toggle').addClass('invisible');
-    }
-  };
-
   // When the user clicks on the button, scroll to the top of the document
-  const scrollToTop = () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  };
+  $('.toggle').click(() => {
+    $('.new-tweet-form').slideDown();
+    $('html, .tweet-area').animate({scrollTop : 0}, 500).focus();
+  });
 });
